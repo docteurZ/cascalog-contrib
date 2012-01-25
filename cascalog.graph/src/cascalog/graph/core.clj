549208602854ea94a95ef21416dec890f3a8eb76
Hugo Zanghi <hugo.zanghi@gmail.com>
2012-01-25 16:24:19 +0100

    add various helper functions

--- cascalog.graph/src/cascalog/graph/core.clj
+++ cascalog.graph/src/cascalog/graph/core.clj
@@ -15,6 +15,16 @@
   (let [line (make-file-source dir)]
     (<- [?dst ?src] (line ?line) (c/re-parse [#"[^\s]+"] ?line :> ?src ?dst) (:distinct false))))
 
+(defmapcatop mk-node
+  [dest src]
+  [[dest] [src]])
+
+(defn enumerate-nodes
+  "enumerate the nodes"
+  [edges]
+  (<- [?node] (edges ?dst ?src) (mk-node ?dst ?src :> ?node) (:distinct true)))
+
+
 (defn in-degree
   "computes the in degrees"
   [edges]
@@ -25,16 +35,34 @@
   [edges]
   (<- [?src ?out_d] (edges _ ?src) (:distinct false) (c/count :> ?out_d)))
 
-(defmapcatop mk-node
-  [dest src]
-  [[dest] [src]])
 
-(defn enumerate-nodes
-  "enumerate the nodes"
+(defn node-degree
+  "computes the degrees"
   [edges]
-  (<- [?node] (edges ?dst ?src) (mk-node ?dst ?src :> ?node) (:distinct true)))
+  (<- [?n ?d] (edges ?dst ?src) (mk-node ?dst ?src :> ?n) (:distinct false) (c/count :> ?d)))
+
+
+;; util functions
 
 (defn count-nodes
   "counts the number of nodes"
   [nodes]
   (<- [?nb-nodes] (nodes ?node ) (c/distinct-count ?node :> ?nb-nodes)))
+
+(deffilterop is-loop?
+  [dest src]
+  (not= dest src))
+
+(defmapop mk-ordered-edge
+  [dest src]
+  (sort [dest src]))
+
+(defn simplifying-graph
+  [edges]
+  (<- [?u ?v] (edges ?dst ?src) (is-loop? ?dst ?src)
+      (mk-ordered-edge ?dst ?src :> ?u ?v)
+      (:distinct true)))
+
+(defmapcatop mk-edge-triangle
+  [u v w]
+  [[u v] [u w] [v w]])
